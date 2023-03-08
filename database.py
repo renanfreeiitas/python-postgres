80eb56d7f1ab32b3467eb4358357a4e0b82ea438
renan <renanfreeiitas@gmail.com>
2023-03-08 11:09:58 +0000

    pegando cursor direto da classe

--- database.py
+++ database.py
@@ -4,14 +4,20 @@ connection_pool = pool.SimpleConnectionPool(1, 1, database="exercises",
                                             user="postgres", password="password", host="localhost")
 
 
-class ConnectionFromPool:
+class CursorFromConnectionFromPool:
     def __init__(self):
         self.connection = None
+        self.cursor = None
 
     def __enter__(self):
         self.connection = connection_pool.getconn()
+        self.cursor = self.connection.cursor()
         return self.connection
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.connection.commit()
+    def __exit__(self, exception_type, exception_value, excepition_traceback):
+        if exception_value is not None:
+            self.connection.rollback()
+        else:
+            self.cursor.close()
+            self.connection.commit()
         connection_pool.putconn(self.connection)
