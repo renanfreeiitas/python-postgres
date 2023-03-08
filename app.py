f035fc682015052cb2b863076cc22a5bb5eaccf4
renan <renanfreeiitas@gmail.com>
2023-03-08 09:59:13 +0000

    deletando linhas de teste

--- app.py
+++ app.py
@@ -7,14 +7,3 @@ my_user.save_to_db()
 user_from_db = User.load_from_db_by_email('renato@gmail.com')
 
 print(user_from_db)
-
-
-
-'''
-apenas testando funcionalidade do codigo junto ao repositorio do github
-'''
-
-
-'''
-novo teste de nuva branch
-'''
